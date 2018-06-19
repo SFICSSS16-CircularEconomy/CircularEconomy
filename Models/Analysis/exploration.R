e6eab18ec344afbe041216fe03d644aab511f176
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-06-19 14:50:51 +0200

    finer_res

--- Models/Analysis/exploration.R
+++ Models/Analysis/exploration.R
@@ -8,7 +8,7 @@ source(paste0(Sys.getenv("CS_HOME"),'/CityNetwork/Models/Utils/R/plots.R'))
 
 #resdirpref='2018_06_16_01_09_28_DIRECTSAMPLING_SYNTHETIC_LOCAL'
 #resdirpref='2018_06_15_18_20_34_DIRECTSAMPLING_SYNTHETIC'
-resdirpref='2018_06_16_21_01_13_DIRECTSAMPLING_SYNTHETIC'
+resdirpref='2018_06_18_12_12_05_DIRECTSAMPLING_SYNTHETIC'
 #res <- as.tbl(read.csv(paste0('explo/',resdirpref,'.csv')))
 res <- as.tbl(read.csv(paste0('exploration/',resdirpref,'.csv')))
 resdir=paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Results/Exploration/',resdirpref,'/');dir.create(resdir)
@@ -16,10 +16,18 @@ resdir=paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Results/Exploration/',resd
 
 res$circularity=1 - res$totalWaste
 res$clusteringLevel = 20 - res$averageDistanceVariability
-res$regime=ifelse(res$totalWaste>0.45,"low","high") # in terms of circularity
+#res$regime=ifelse(res$totalWaste>0.45,"low","high") # in terms of circularity
 
-#trcost=3.5
-trcost=0.5
+
+
+#####
+## Stochasticity
+
+
+
+
+trcost=3.5
+#trcost=0.5
 
 
 ## Question : determinants of output regime ?
@@ -51,10 +59,10 @@ ggsave(file=paste0(resdir,'totalWaste_facetsd-overlap_trCost',trcost,'.png'),wid
 # nicer plot
 #res$sigma = unlist(sapply(res$distribSd,function(s){bquote(sigma*'='*.(s))}))
 # trick the regime when few points only
-res$regime[res$overlapThreshold==0&res$distribSd==0.6]="high"
+#res$regime[res$overlapThreshold==0&res$distribSd==0.6]="high"
 #g=ggplot(res[res$transportationCost==trcost&res$overlapThreshold%in%c(0,0.5),],aes(x=clusteringLevel,y=circularity,group=interaction(gravityDecay,regime),color=gravityDecay,linetype=regime))
-g=ggplot(res[res$transportationCost==trcost&res$overlapThreshold%in%c(0.01,0.05),],aes(x=clusteringLevel,y=circularity,group=gravityDecay,color=gravityDecay))
 
+g=ggplot(res[res$transportationCost==trcost&res$overlapThreshold%in%c(0.0,0.05)&res$distribSd%in%c(0.01,0.05),],aes(x=clusteringLevel,y=circularity,group=gravityDecay,color=gravityDecay))
 g+geom_point(pch='.')+geom_smooth()+facet_grid(distribSd~overlapThreshold,scales="free")+
   xlab('Level of clustering')+ylab('Level of circularity')+scale_color_continuous(name=expression(d[0]))+stdtheme
 ggsave(file=paste0(resdir,'totalWaste_facetsd-overlap_trCost',trcost,'_extract_withRegime.png'),width=18,height=15,units='cm')
