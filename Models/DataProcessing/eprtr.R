defba8d0c18a1f0272379804dc30679ce4345e8c
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-06-03 15:35:51 +0200

    fix_nullresults

--- Models/DataProcessing/eprtr.R
+++ Models/DataProcessing/eprtr.R
@@ -4,9 +4,8 @@ setwd(paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Models/DataProcessing'))
 library(dplyr)
 
 
-#transfer <- as.tbl(read.csv(paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Data/EPRTR/E-PRTR_database_v16_csv/dbo.PUBLISH_WASTETRANSFER.csv'),fileEncoding='latin1'))
-#activities <- as.tbl(read.csv(paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Data/EPRTR/E-PRTR_database_v16_csv/dbo.PUBLISH_ACTIVITY.csv')))
-facilities <- as.tbl(read.csv(paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Data/EPRTR/E-PRTR_database_v16_csv/dbo.PUBLISH_FACILITYREPORT.csv'),fileEncoding='latin1'))
+transfer <- as.tbl(read.csv(paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Data/EPRTR/E-PRTR_database_v16_csv/dbo.PUBLISH_WASTETRANSFER.csv'),fileEncoding='latin1'))
+facilities <- as.tbl(read.csv(paste0(Sys.getenv('CS_HOME'),'/CircularEconomy/Data/EPRTR/E-PRTR_database_v16_csv/dbo.PUBLISH_FACILITYREPORT.csv')))#,fileEncoding='latin1'))
 
 
 #unique(as.character(transfer$WasteHandlerPartyAddressCity))
@@ -18,31 +17,10 @@ codes = unique(paste0(as.character(facilities$CountryCode)," ",as.character(faci
 codes = codes[nchar(codes) > 3&codes!="CONFIDENTIAL CONFIDENTIAL"]
 
 
-library(nominatim)
 
-#osm_geocode(codes[1:10],key='GmFUKXAg4vykLxJOx3Pzt7KGJXKuKG9b')
 
-n = length(codes)
-#n=10
+load('geocode_facilities.RData')
 
-key=as.character(read.csv('apikey',header=F)[1,1])
 
-names=c();lon=c();lat=c();
-inds=c()
-for(i in 1:n){
-  show(paste0(i,"/",n))
-  searchres = osm_search(codes[i],key=key)
-  show(codes[i])
-  show(searchres)
-  names=append(names,searchres$display_name)
-  lon=append(lon,searchres$lon)
-  lat=append(lat,searchres$lat)
-  inds=append(inds,i)
-  Sys.sleep(2)
-}
-
-#res = data.frame(idrow = 1:n,code = codes[1:n],name = names,lon = lon,lat = lat)
-#save(inds,names,lon,lat,file='geocode_wastehandler.RData')
-save(inds,names,lon,lat,file='geocode_facilities.RData')
 
 
